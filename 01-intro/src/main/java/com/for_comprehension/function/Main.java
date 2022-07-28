7cf36a99c58effd115b6380d14de770b6c2c2fb5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-07-28 14:51:25 +0200

    Streams Collectors: Examples

--- 01-intro/src/main/java/com/for_comprehension/function/Main.java
+++ 01-intro/src/main/java/com/for_comprehension/function/Main.java
@@ -3,10 +3,7 @@ package com.for_comprehension.function;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  *
@@ -14,9 +11,6 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        IntStream intStream = IntStream.of(1);
-        Stream<Integer> boxed = intStream.boxed();
-        IntStream intStream1 = boxed.mapToInt(i -> i);
     }
 
     public static int run(Supplier<Integer> run) {
--- 01-intro/src/main/java/com/for_comprehension/function/l2_stream/ApiOverview4Collect.java
+++ 01-intro/src/main/java/com/for_comprehension/function/l2_stream/ApiOverview4Collect.java
@@ -1,12 +1,21 @@
 package com.for_comprehension.function.l2_stream;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toMap;
 
 public class ApiOverview4Collect {
 
@@ -22,5 +31,33 @@ public class ApiOverview4Collect {
         LinkedList<Integer> llist = Stream.of(1, 2, 3)
             .map(i -> i + 1)
             .collect(toCollection(LinkedList::new));
+
+        TreeMap<String, Integer> collect = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(toMap(s -> s, String::length, (o, o2) -> o, TreeMap::new));
+
+        Map<Integer, List<String>> groupingBy = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(groupingBy(String::length));
+
+        TreeMap<Integer, Long> groupingByCounting = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(groupingBy(String::length, TreeMap::new, Collectors.counting()));
+
+        Map<String, Boolean> collect2 = Stream.of("a", "aa", "bb", "ccc")
+            .collect(toMap(s -> s, s -> s.length() > 2));
+
+        Map<Boolean, List<String>> collect1 = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(partitioningBy(s -> s.length() > 2));
+
+        // anti example
+        Map<Integer, List<String>> collect3 = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(toMap(String::length, List::of, (o, o2) -> Stream.concat(o.stream(), o2.stream()).collect(Collectors.toList())));
+
+        String joined = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(Collectors.joining(",", "{", "}"));
+
+        String joinedUpper = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(collectingAndThen(joining(), String::toUpperCase));
+
+        List<String> collect4 = Stream.of("a", "a", "aa", "bb", "ccc")
+            .collect(Collectors.toUnmodifiableList());
     }
 }
