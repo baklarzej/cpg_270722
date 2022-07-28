44af31033ddbe58d28701110a6801c30255ffc62
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-07-28 09:09:20 +0200

    Supplier to Callable

---
+++ 01-intro/src/main/java/com/for_comprehension/function/lambda/SupplierToCallable.java
@@ -0,0 +1,40 @@
+package com.for_comprehension.function.lambda;
+
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class SupplierToCallable {
+
+    public static void main(String[] args) {
+        Supplier<Integer> supplier = () -> 42;
+
+        // 1
+        Callable<Integer> callable1 = new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                return 42;
+            }
+        };
+
+        // 2
+        Callable<Integer> callable2 = new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                return supplier.get();
+            }
+        };
+
+        // 3
+        Callable<Integer> callable3 = () -> supplier.get();
+
+        // 4
+        Callable<Integer> callable4 = supplier::get;
+
+        // 5
+        Function<Supplier<Integer>, Callable<Integer>> f = s -> s::get;
+    }
+
+}
