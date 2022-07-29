e3a22f28d74194d5787c46162a97050a3e77eb91
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-07-29 10:22:45 +0200

    ExecuteAroundExecutionTimeLogging

--- 01-intro/src/main/java/com/for_comprehension/function/l3_execute_around/ExecuteAroundExecutionTimeLogging.java
+++ 01-intro/src/main/java/com/for_comprehension/function/l3_execute_around/ExecuteAroundExecutionTimeLogging.java
@@ -1,23 +1,63 @@
 package com.for_comprehension.function.l3_execute_around;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.function.Supplier;
 
 public class ExecuteAroundExecutionTimeLogging {
 
     public static void main(String[] args) {
-        int result = timed(() -> compute());
-        System.out.println(result);
+        withLogging(withTiming(() -> compute())).get();
+
+        System.out.println("-----");
+
+        timed(() -> compute());
     }
 
-    public static int compute() {
+    public static <T> Supplier<T> withLogging(Supplier<T> runnable) {
+        return () -> {
+            System.out.println("Entering method");
+            T result = runnable.get();
+            System.out.println("Exiting method");
+            return result;
+        };
+    }
+
+    public static <T> Supplier<T> withTiming(Supplier<T> function) {
+        return () -> {
+            var before = Instant.now();
+            T result = function.get();
+            var after = Instant.now();
+
+            long millis = Duration.between(before, after).toMillis();
+
+            System.out.println("Took: " + millis + "ms");
+
+            return result;
+        };
+    }
+
+    public static <T> T timed(Supplier<T> function) {
+        var before = Instant.now();
+        T result = function.get();
+        var after = Instant.now();
+
+        long millis = Duration.between(before, after).toMillis();
+
+        System.out.println("Took: " + millis + "ms");
+
+        return result;
+    }
+
+    public static String compute() {
         System.out.println("calculating...");
         try {
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         }
         catch (InterruptedException e) {
         }
         System.out.println("finished");
-        return ThreadLocalRandom.current().nextInt();
+        return "42";
     }
 
 }
